048f549657fadafc902615be7831c48e711f2032
Bhagwan-Sonu <bhagwansingh760@gmail.com>
2025-07-19 18:35:23 +0530

    second push

--- src/main/java/com/enotes/controller/CategoryController.java
+++ src/main/java/com/enotes/controller/CategoryController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,6 +54,26 @@ public class CategoryController {
 			return ResponseEntity.noContent().build();
 		}else {
 			return new ResponseEntity<>(activeCategory, HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getCategoryDetailsById(@PathVariable Integer id){
+		CategoryDto categoryDto = categoryService.getCategoryById(id);
+		if(ObjectUtils.isEmpty(categoryDto)) {
+			return new ResponseEntity<>("Category not found By Id", HttpStatus.NOT_FOUND);
+		}else {
+			return new ResponseEntity<>(categoryDto, HttpStatus.OK);
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteCategoryById(@PathVariable Integer id){
+		Boolean deleted = categoryService.deleteCategoryById(id);
+		if(deleted) {
+			return new ResponseEntity<>("Category deleted successfully", HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("Category not found.", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
